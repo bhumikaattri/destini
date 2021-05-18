4c4a997e7f33ed6453e243389514769bea9e4f88
bhumikaattri <80252434+bhumikaattri@users.noreply.github.com>
2021-05-18 15:20:08 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+void main() {
+  runApp(Destini()
+  );
+}
+
+class Destini extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    theme: ThemeData.dark(),
+    home: StoryPage(),
+    );
+  }
+}
+StoryBrain storyBrain =StoryBrain();
+class StoryPage extends StatefulWidget {
+  @override
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/destini.png'),
+            fit: BoxFit.cover, 
+            ),
+        ),
+        padding: EdgeInsets.symmetric(vertical:50.0,horizontal:15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child:Column(
+            crossAxisAlignment:CrossAxisAlignment.stretch,
+             children: <Widget>[
+                Expanded(
+                  flex:12,
+                  child: Center(
+                    child: Text(
+                      storyBrain.getStory(),
+                     style:TextStyle(
+                      fontSize:20.0,
+                     ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex:2,
+                  child:TextButton(
+                    onPressed:(){
+                      setState(() {
+                        storyBrain.nextStory(1);
+                      });
+                    },
+                    style: TextButton.styleFrom(backgroundColor:Colors.redAccent[700],),
+                    child: Text(
+                      storyBrain.getChoice1(),
+                      style: TextStyle(
+                      fontSize:20.0,
+                      color:Colors.black,
+                    ),),
+                  ),
+                ),
+                SizedBox(
+                  height:20.0,
+                ),
+                Expanded(
+                  flex:2,
+                  child:Visibility(visible: storyBrain.buttonShouldBeVisible(),
+                  child:TextButton(
+                    onPressed:(){
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    style: TextButton.styleFrom(backgroundColor:Colors.blueAccent[700],),
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                      fontSize:20.0,
+                      color: Colors.black,
+                    ),),
+                  ),
+                  ),
+                ),
+                
+             ],
+          ) ,
+          ),
+
+      )
+    );
+      
+    
+  }
+}
